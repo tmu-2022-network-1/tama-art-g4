47f6c4d1929f67e316ce4c1cc1755b774f57ce79
imayoshi yuki <103159800+imayoshiyuki@users.noreply.github.com>
2022-06-13 09:40:19 +0900

    会場情報の追加（jsを結構書き換えています）

--- docs/modules/getdata.js
+++ docs/modules/getdata.js
@@ -1,12 +1,15 @@
-export async function getData(sheetName = '') {
+export async function getData(sheetName1 = '', sheetName2 = '') {
     const endpoint =
       "https://script.google.com/macros/s/AKfycbxYb6A56yxS_gLG_AkWxMODItAzBrzYYT8CT3Yvxel3UlgNhau-sJnH1ZbFM-Ho_GcQkA/exec";
-      console.log(endpoint + "?sheet=" + sheetName);
+      console.log(endpoint + "?sheet=" + sheetName1);
     try {
-      const response = await fetch(`${endpoint}?sheet=${sheetName}`);
-      if (response.ok) {
-        const json = await response.json();
-        return json;
+      const response1 = await fetch(`${endpoint}?sheet=${sheetName1}`);
+      const response2 = await fetch(`${endpoint}?sheet=${sheetName2}`);
+      if (response1.ok && response2.ok) {
+        const eventJson = await response1.json();
+        const venueJson = await response2.json();
+        // console.log(eventJson);
+        return [eventJson,venueJson];
       }
     } catch (error) {
       console.log(error);
