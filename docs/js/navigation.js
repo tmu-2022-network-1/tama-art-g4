15116c8483a0ec8de6da409b668ebf285fc9a908
yuirin83 <yuirin83@gmail.com>
2022-06-15 22:11:44 +0900

    タグ選択画面の調整

--- docs/js/navigation.js
+++ docs/js/navigation.js
@@ -1,15 +1,34 @@
 
 $(".right-text").click(function () {//ボタンがクリックされたら
-    if((this).classList.contains('active') == true ){
-        (this).textContent = '- other tag -';
-    }else{
-        (this).textContent = '- close -';
-    }
+    // if((this).classList.contains('active') == true ){
+    //     (this).textContent = '- other tag -';
+    // }else{
+    //     (this).textContent = '- close -';
+    // }
     $("#right-fixed").toggleClass('.right-fixed-z-index');
     $(".right-text").toggleClass('active');
     $("#g-nav").toggleClass('panelactive');
+    
+    var state = openNav();
+    document.getElementById('tag-state').textContent = '-' + state + '-';
 });
 
+$(".radio-inline__label").click(function () {
+    $("#right-fixed").toggleClass('.right-fixed-z-index');
+    $(".right-text").toggleClass('active');
+    $("#g-nav").toggleClass('panelactive');
+
+    var state = openNav();
+    document.getElementById('tag-state').textContent = '-' + state + '-';
+});
+
+var openNav = function(){
+    if($("#g-nav").hasClass('panelactive')){
+        return 'close';
+    }else{
+        return 'other tag';
+    }
+}
 
 $(".close").click(function () {//ボタンがクリックされたら
     console.log("mumu");
